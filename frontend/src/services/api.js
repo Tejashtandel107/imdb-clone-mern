8e8ee88d3e673099d09a661aeb20980cfe2ac4ec
tejashtandel <tejas@itidoltechnologies.com>
2025-12-26 04:29:29 +0530

    revert last changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.API_URL || 'https://imdb-clone-mern.onrender.com/';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
